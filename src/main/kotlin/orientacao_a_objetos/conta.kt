351a4b688723847626181a9acb063de44795603f
othonbreener <othonbrener123@gmail.com>
2022-06-02 13:19:06 -0300

    Continuando o assunto sobre orientação a objetos.
    Adicionando trativa para saldo e método para transferir entre contas.

--- src/main/kotlin/orientacao_a_objetos/conta.kt
+++ src/main/kotlin/orientacao_a_objetos/conta.kt
@@ -11,17 +11,37 @@ data class ContaCorrente(
     val numeroConta: Int,
     var saldo: Double
 ): Conta {
+
     override fun sacar(valor: Double): String {
-        this.saldo -= valor
-        return "Saldo atual: ${this.saldo}"
+        return if (this.saldo < valor) {
+            "Saldo insuficiente, saldo atual: ${this.saldo}"
+        } else {
+            this.saldo -= valor
+            "Saldo atual: ${this.saldo}"
+        }
     }
+
     override fun depositar(valor: Double): String {
         this.saldo += valor
         return "Saldo atual: ${this.saldo}"
     }
-    override fun transferir (valor: Double, numeroConta: Int): String {
-        this.saldo -= valor
 
-        return "Valor transferido para: $numeroConta, Saldo atual: ${this.saldo}"
+    override fun transferir (valor: Double, numeroConta: Int): String {
+
+        var transferido = false
+        for (conta in contas) {
+            if (conta.numeroConta == numeroConta) {
+                conta.saldo += valor
+                transferido = true
+            }
+        }
+        return if (!transferido) {
+            "Conta $numeroConta não encontrada!"
+        } else {
+            this.saldo -= valor
+            "Valor transferido para: $numeroConta, Saldo atual: ${this.saldo}"
+        }
     }
 }
+
+val contas = mutableListOf<ContaCorrente>()
--- src/main/kotlin/orientacao_a_objetos/main.kt
+++ src/main/kotlin/orientacao_a_objetos/main.kt
@@ -1,7 +1,17 @@
 package orientacao_a_objetos
 
 fun main() {
+    val conta_1 = ContaCorrente("Lucas", 1001, 5200.0)
+    val conta_2 = ContaCorrente("Matheus", 1002, 5300.0)
     val conta = ContaCorrente("Othon", 1000, 5000.0)
+
+    contas.add(conta_1)
+    contas.add(conta_2)
+    contas.add(conta)
+
     println(conta.sacar(1000.0))
+    println(conta.sacar(5000.0))
     println(conta.depositar(520.31))
+    println(conta.transferir(1000.0, 1001))
+    println(conta_1.saldo)
 }
